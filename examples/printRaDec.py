96f523f122a02499136742e4a982f3b2357dcdd6
haydengcrisp <20934203@student.uwa.edu.au>
2018-11-01 13:56:31 +0800

    commented out gal.name until that works

--- examples/printRaDec.py
+++ examples/printRaDec.py
@@ -20,7 +20,7 @@ for file in os.scandir(directory):
 		print(f'{directory}{file.name}')
 		gal = SDSSData(f'{directory}{file.name}') 
 		#assert gal.name is not None, f'{file.name} has no name data! Please check the file and try again.'
-		assert gal.ra is not None, f'{file.name} has no RA data! Please check the file and try again.' 
-		assert gal.dec is not None, f'{file.name} has no dec data! Please check the file and try again.' 
+		assert gal.ra is not None, f'file.name has no RA data! Please check the file and try again.' 
+		assert gal.dec is not None, f'file.name has no dec data! Please check the file and try again.' 
  
 		print(f'Galaxy gal.name is located at RA {gal.ra}%.2f and dec {gal.dec}%.2f.')
