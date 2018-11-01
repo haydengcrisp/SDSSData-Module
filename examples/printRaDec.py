4deb88147f21ca2ae8e31193d6197bfb33ff3dbf
haydengcrisp <20934203@student.uwa.edu.au>
2018-11-01 11:56:03 +0800

    added assert statements to check file quality and appropriate syntax for SDSS object

--- examples/printRaDec.py
+++ examples/printRaDec.py
@@ -2,19 +2,21 @@
 This is a script to print the name, RA and dec for all galaxies in /data/spectra
 '''
 
-import SDSSData as sdss
+#import SDSSData
 import os
 
 #find the spectra directory over which we will iterate
 
-directory = '../data/spectra'
+directory = '../data/spectra/'
 
 for file in os.scandir(directory):
 	if '.fits' in file.name:
 		print(file.name)
-
-		gal = sdss.Thing(file.name) #???
-
-		gal.ra
-		gal.dec
-		gal.name
+		print(f'{directory}{file.name}')
+		assert gal.name != None f'{file.name} has no name data! Please check the file and try again.'
+		assert gal.ra != None f'{file.name} has no RA data! Please check the file and try again.' 
+		assert gal.dec != None f'{file.name} has no dec data! Please check the file and try again.' 
+ 
+#		gal = SDSSData(f'{directory}{file.name}') 
+#
+#		print(f'Galaxy {gal.name} is located at RA {gal.ra}%.2f and dec {gal.dec}%.2f.')
